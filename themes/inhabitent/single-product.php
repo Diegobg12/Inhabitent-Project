afe8ed23bfe5f7425a1f373427cacf8d82ca5974
Diego Bustos <diegobustos1229@gmail.com>
2020-01-23 00:36:09 -0800

    fix single product

--- themes/inhabitent/single-product.php
+++ themes/inhabitent/single-product.php
@@ -11,7 +11,7 @@ get_header(); ?>
    <main id="main" class="site-main " role="main">
    <?php while ( have_posts() ) : the_post(); ?>
 
-   <article class="hentry">
+   <article class="single-product">
          <div class="product-image-wrapper">
             <img  class="wp-post-image" src="<?php echo CFS()->get('image'); ?>" alt="">
          </div>
