a85bdd54d9d9b4427a2efe2777467b001f9c4cff
Diego Bustos <diegobustos1229@gmail.com>
2020-01-24 23:40:44 -0800

    single name product taxonomy

--- themes/inhabitent/archive.php
+++ themes/inhabitent/archive.php
@@ -14,8 +14,8 @@ get_header(); ?>
 
 			<header class="page-header">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+				single_term_title( '<h1 class="page-title">', '</div>' );
+				the_archive_description( '<div class="taxonomy-description">', '</div>' );
 				?>
 			</header><!-- .page-header -->
 
