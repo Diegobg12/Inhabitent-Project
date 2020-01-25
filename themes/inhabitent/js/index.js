5618e350fdc6958f5e4522671d8dc9e8f5879ac1
Diego Bustos <diegobustos1229@gmail.com>
2020-01-24 16:09:02 -0800

    fix search bar on 404 page

--- themes/inhabitent/js/index.js
+++ themes/inhabitent/js/index.js
@@ -1,17 +1,43 @@
 
 $( document ).ready(function() {
+    const inputHeader= $('input:first');
+    const inputSecond= $('.search-field').last();
+
+    $(inputSecond).removeClass('hide');
+    $(inputHeader).addClass('hide');
+
     $('.search-toggle').click(function(){
         event.preventDefault();
-      $('.search-field').show('slow');
-      $('.search-field').focus();
+      $(inputHeader).show('slow');
+      $(inputHeader).focus();
     });
 
-    $('.search-field').focusout(function(){
+    $(inputHeader).focusout(function(){
         event.preventDefault();
-        $('.search-field').hide('slow');
+        $(inputHeader).hide('slow');
     });
-    
+
   });
 
-  
+
+// $(window).scroll(function() {    
+//   var scroll = $(window).scrollTop();
+//   var mainPage = $( "#masthead" ).hasClass( "home-site-header" );
+//    //>=, not <=
+
+//       if ((scroll >= 700) && !mainPage ){
+//           //clearHeader, not clearheader - caps H
+//           // $("#masthead").removeClass();
+//           $("#masthead").addClass("site-header");
+//       }else{
+//         $("#masthead").removeClass("site-header");
+//         // $("#masthead").addClass("home-site-header");
+//       }
+   
+
+}); 
+
+
+
+
   
--- themes/inhabitent/searchform.php
+++ themes/inhabitent/searchform.php
@@ -1,11 +1,15 @@
 <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
 	<fieldset>
+
+	
 		<a href="#" class="search-toggle" aria-hidden="true">
 				<i class="fa fa-search"></i>
 		</a>
 
+
+
 		<label>
-			<input type="search" class="search-field hide" placeholder="Type and hit enter..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
+			<input type="search" class="search-field" placeholder="Type and hit enter..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
 
 		<input type="submit" id="search-submit" class="screen-reader-text" value="Search">
